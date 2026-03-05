e5da02e9a9466a803aa7bfee1c536bc28fcd429b
oopsoleander <harshworkau@gmail.com>
2026-03-05 13:18:30 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
-import React from 'react';
+import Navbar from "./components/Navbar"
+import Hero from "./components/Hero"
+import AnimatedStats from "./components/AnimatedStats"
+import Events from "./components/Events"
+import FAQ from "./components/FAQ"
+import Footer from "./components/Footer"
+import LiveBookings from "./components/LiveBookings"
 
-class App extends React.Component {
-    render() {
-        return (
-            <div>
-                <h1>Welcome to CLUVIA</h1>
-                <p>This is the main application component.</p>
-            </div>
-        );
-    }
+export default function App(){
+
+return(
+
+<div>
+
+<Navbar/>
+
+<Hero/>
+
+<AnimatedStats/>
+
+<Events/>
+
+<FAQ/>
+
+<Footer/>
+
+<LiveBookings/>
+
+</div>
+
+)
+
 }
-
-export default App;
