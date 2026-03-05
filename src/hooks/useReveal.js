dbb5f6f063adeaa205e51d618d18875abce32e39
oopsoleander <harshworkau@gmail.com>
2026-03-05 13:39:09 +0530

    Create useReveal.js

---
+++ src/hooks/useReveal.js
@@ -0,0 +1,21 @@
+import { useEffect } from "react"
+
+export default function useReveal(){
+
+useEffect(()=>{
+
+const observer = new IntersectionObserver(entries=>{
+entries.forEach(entry=>{
+if(entry.isIntersecting){
+entry.target.classList.add("visible")
+}
+})
+})
+
+document.querySelectorAll(".reveal").forEach(el=>{
+observer.observe(el)
+})
+
+},[])
+
+}
