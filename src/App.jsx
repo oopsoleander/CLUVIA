8d46f542e6daa1a8c7dfb0379307e81d2139f980
oopsoleander <harshworkau@gmail.com>
2026-03-05 14:15:20 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,7 @@
-import Navbar from "./components/Navbar"
-import Hero from "./components/Hero"
-import AnimatedStats from "./components/AnimatedStats"
-import Events from "./components/Events"
-import FAQ from "./components/FAQ"
-import Footer from "./components/Footer"
-import LiveBookings from "./components/LiveBookings"
-
-export default function App(){
-
-return(
-
-<div>
-
-<Navbar/>
-
-<Hero/>
-
-<AnimatedStats/>
-
-<Events/>
-
-<FAQ/>
-
-<Footer/>
-
-<LiveBookings/>
-
-</div>
-
-)
-
+export default function App() {
+  return (
+    <div style={{color:"white",padding:"40px"}}>
+      CLUVIA WORKING 🚀
+    </div>
+  )
 }
