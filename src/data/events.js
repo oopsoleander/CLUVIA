b54b31c95ef680b007064193a373594669bccb5a
oopsoleander <harshworkau@gmail.com>
2026-03-05 13:21:17 +0530

    Update events.js

--- src/data/events.js
+++ src/data/events.js
@@ -1,20 +1,21 @@
-const events = [
-    // Sample event objects
-    {
-        id: 1,
-        title: 'Event 1',
-        date: '2026-03-10',
-        location: 'Location 1',
-        description: 'Description for Event 1'
-    },
-    {
-        id: 2,
-        title: 'Event 2',
-        date: '2026-03-12',
-        location: 'Location 2',
-        description: 'Description for Event 2'
-    },
-    // Add more events as needed
-];
+export const events=[
 
-export default events;
+{
+name:"VELVET Saturdays",
+venue:"The Grand Delhi",
+spots:24
+},
+
+{
+name:"Ladies Night Deluxe",
+venue:"Elevate Delhi",
+spots:18
+},
+
+{
+name:"Playboy Blackout",
+venue:"Playboy Club",
+spots:12
+}
+
+]
