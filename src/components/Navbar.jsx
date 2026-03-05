73daab285201a7d1b290e63c2fb56cdd3302a744
oopsoleander <harshworkau@gmail.com>
2026-03-05 12:39:56 +0530

    Create Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class Navbar extends React.Component {
+    render() {
+        return (
+            <nav>
+                <h1>CLUVIA</h1>
+                {/* Add navigation links here */}
+            </nav>
+        );
+    }
+}
+
+export default Navbar;
