d33d493b5763d4fbeb2ddaa34bb231a4e3cbe12b
oopsoleander <harshworkau@gmail.com>
2026-03-05 12:40:17 +0530

    Creating main application component in App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class App extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Welcome to CLUVIA</h1>
+                <p>This is the main application component.</p>
+            </div>
+        );
+    }
+}
+
+export default App;
